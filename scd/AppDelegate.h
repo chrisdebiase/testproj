30cb751cc2de62b0e770e4ed387c2e8bdcf84ff8
Chris <cdebiase@college.harvard.edu>
2015-11-24 17:20:01 -0500

    Initial Commit

---
+++ scd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scd
+//
+//  Created by Chris DeBiase on 11/24/15.
+//  Copyright © 2015 Chris DeBiase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
